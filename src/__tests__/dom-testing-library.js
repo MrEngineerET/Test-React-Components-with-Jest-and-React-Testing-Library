78cdc4b3335501df40a193cd5d05398ffd64fcc1
Biruk Berhanu <meetbirukberhanu@gmail.com>
2022-12-18 16:56:19 +0300

    refactor dom testing libray code

--- src/__tests__/dom-testing-library.js
+++ src/__tests__/dom-testing-library.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // eslint-disable-next-line testing-library/no-dom-import
-import {queries} from '@testing-library/dom'
+import {getQueriesForElement} from '@testing-library/dom'
 import {FavoriteNumber} from '../favorite-number'
 
 test("render a number input with a 'Favorite Number' label", () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
 
+  const {getByLabelText} = getQueriesForElement(div)
+
   // eslint-disable-next-line testing-library/prefer-screen-queries
-  const input = queries.getByLabelText(div, 'Favorite Number')
+  const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
 })
